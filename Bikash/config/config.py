d2019b91e8aab5e9567b5d9a5d33c95607bd5444
kakarot-6969 <rajsrijan51@gmail.com>
2024-07-27 01:11:48 -0700

    Update config.py

--- Bikash/config/config.py
+++ Bikash/config/config.py
@@ -9,22 +9,22 @@ from pyrogram import filters
 if os.path.exists("Internal"):
   load_dotenv("Internal")
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_ID = int(getenv("BOT_ID", "7198638721"))
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv(" 21899392"))
+API_HASH = getenv("1784e0b77aa020165687f59f573ac783")
+BOT_ID = int(getenv("", ""))
+BOT_TOKEN = getenv("7237043913:AAH531b1m0ywz33zX-OgBE2dGl_7n6FenHk")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "༺ 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 ༻")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1439222689").split()))
+MONGO_DB_URI = getenv("mongodb+srv://kakarot6060:kakarot6060@cluster0.lgzyjfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
+LOG_GROUP_ID = int(getenv("-1002147625936"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝐘ᴏʀ 𝐅ᴏʀɢᴇʀ")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6269731241").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BikashHalderNew/Bgtplayer")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "bikash")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BikashGadgetsTech")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Bgt_Chat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/channel_work_bots")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/channel_work_bots")
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "180"))
 QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", "150"))
@@ -49,7 +49,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 SET_CMDS = getenv("SET_CMDS", False)
 # https://www.gbmb.org/mb-to-bytes
 
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("BQFOKIAACEchzcdelhq8aPZNijN5w6BuSWOusgn7TrObe6jdtMpuvHQGd-o6FB7Vgvc9HnIsorJH8Aot-66shKyQiAcznhCLUcJkv5XLoqce8XWOE2KKRlLVeLsCquchj8aAgfFAPR2epsid6ISdiatYpdx6Z-JCTy6LiQaO6yDAlLoj6JhASty3XmTdRmAcacaDu27ukVI1XiVVyEVAHJA62wkOhlkqmFujB39T9vbn0AiDbITjPHCMyou_2aPjEoPT37Dr8dtGpm6T7kRwVhyAmn58Ll7vcUVDM1lHxDfD9ixes8Jg9vaI0ENeg4iuqleBzXq47g22IAIUV_6eio-BK78t6gAAAAG62_5DAA", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -57,7 +57,7 @@ STRING5 = getenv("STRING_SESSION5", None)
 
 ############################
 COMMAND_PREFIXES.append('')
-OWNER_ID.append(1439222689)
+OWNER_ID.append(6269731241)
 ############################
 BANNED_USERS = filters.user()
 YTDOWNLOADER = 1
@@ -71,20 +71,20 @@ clean = {}
 autoclean = []
 ############################
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/3127b3484676bd2254e97.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://telegra.ph/file/396c004bbe73f4b808617.jpg")
 
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg"
-GLOBAL_IMG_URL = "https://te.legra.ph/file/2e2741f5dfe9f62eed91d.png"
-STATS_IMG_URL = "https://te.legra.ph/file/2be94fc76030833b51a78.png"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/d68f9ff85714d4dbc6069.png"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/d68f9ff85714d4dbc6069.png"
-STREAM_IMG_URL = "https://graph.org/file/93882ae5ea01a7bf687b1.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/0d021735560cbf0bb749a.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/4a8cc770a5bea2136bada.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/8427fca139bcbf3c54bcb.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+GLOBAL_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "hhttps://telegra.ph/file/396c004bbe73f4b808617.jpg"
 
 
 def time_to_seconds(time):
@@ -110,9 +110,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "resources/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/3127b3484676bd2254e97.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/396c004bbe73f4b808617.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "resources/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/c8135608b7f9f77541a82.jpg"
+            START_IMG_URL = "https://telegra.ph/file/75314f38cc16cb479a9b7.jpg"
